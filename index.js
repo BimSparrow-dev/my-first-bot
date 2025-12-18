e5533ab568ba86aab27401317a08bab941994ce6
BimSparrow-dev <kareemfyfield2@gmail.com>
2025-12-18 17:27:33 -0400

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-console.log("Hello from my first bot!");
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.once("ready", () => {
+  console.log("Bot is online!");
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === "!hello") {
+    message.reply("Hello 👋");
+  }
+});
+
+client.login(process.env.TOKEN);
+
 
 
